11341a19658cd621a66cb2b733da0f238c3b3870
Luuuuna <vvaskaa@list.ru>
2024-02-21 14:29:05 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+abstract class Resource<T> {
+  protected data: T[];
+
+  constructor(data: T[]) {
+    this.data = data;
+  }
+
+  get(): T[] {
+    return this.data;
+  }
+
+  getOne(key: keyof T, value: any): T | undefined {
+    return this.data.find((item) => item[key] === value);
+  }
+
+  add(newObj: T): T[] {
+    this.data.push(newObj);
+    return this.data;
+  }
+
+  update(key: keyof T, value: any, partialData: Partial<T>): T | undefined {
+    const index = this.data.findIndex((item) => item[key] === value);
+    if (index !== -1) {
+      this.data[index] = { ...this.data[index], ...partialData };
+      return this.data[index];
+    }
+    return undefined;
+  }
+
+  delete(key: keyof T, value: any): T | undefined {
+    const index = this.data.findIndex((item) => item[key] === value);
+    if (index !== -1) {
+      const deletedItem = this.data.splice(index, 1)[0];
+      return deletedItem;
+    }
+    return undefined;
+  }
+}
+
+
+
+
+
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
+interface Order {
+  id: number;
+  userId: number;
+  product: string;
+  quantity: number;
+}
+class UserModel extends Resource<User> {
+}
+class OrderModel extends Resource<Order> {
+}
+const users: User[] = [
+  { id: 1, name: "John", age: 25 },
+  { id: 2, name: "Jane", age: 30 },
+  { id: 3, name: "Bob", age: 35 }
+];
+const orders: Order[] = [
+  { id: 1, userId: 1, product: "Book", quantity: 2 },
+  { id: 2, userId: 2, product: "Phone", quantity: 1 },
+  { id: 3, userId: 3, product: "TV", quantity: 1 }
+];
+const userModel = new UserModel(users);
+const orderModel = new OrderModel(orders);
+console.log(userModel.get()); 
+console.log(orderModel.get()); 
+console.log(userModel.getOne('id', 2)); 
+console.log(orderModel.getOne('product', 'Phone')); 
+console.log(userModel.add({ id: 4, name: 'Alice', age: 40 })); 
+console.log(orderModel.add({ id: 4, userId: 1, product: 'Headphones', quantity: 1 })); 
+console.log(userModel.update('id', 2, { age: 31 })); 
+console.log(orderModel.update('userId', 3, { product: 'Laptop' })); 
+console.log(userModel.delete('id', 1)); 
+console.log(orderModel.delete('id', 2)); 
